11d2cd0c80430d84ddc9ae0f5da29a85e3e699b1
Harihar Padhi <39830449+1515236544hit@users.noreply.github.com>
2023-08-21 21:29:30 +0530

    Created a nodejs based crud operations

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { APP_PORT } from "./config";
+import routes from "./routes";
+import errorHandler from "./middlewares/errorHandler";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", routes);
+
+app.use(errorHandler);
+
+app.listen(APP_PORT, () => console.log(`Listen to port ${APP_PORT}`));
